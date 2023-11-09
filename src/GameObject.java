06531a5bfbe9ab9ea5496ad8099dbef8dc4e1db8
Ilan ATTIAS <CobraClown09300@outlook.fr>
2023-11-09 14:44:12 +0100

    [Update | GameObject] Add getter for x and y

--- src/GameObject.java
+++ src/GameObject.java
@@ -97,7 +97,15 @@ public class GameObject {
         this.x = x;
     }
 
+    public double getX() {
+        return x;
+    }
+
     public void setY(double y) {
         this.y = y;
+    }
+
+    public double getY() {
+        return y;
     }
 }

eed42b181614df227f0e789bc48f22555ff82785
Ilan ATTIAS <CobraClown09300@outlook.fr>
2023-11-10 16:03:01 +0100

    [Update | Mario] Comment out many of the method to concentrate on base movements

--- src/Mario.java
+++ src/Mario.java
@@ -1,175 +1,147 @@
-
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
-import javax.swing.JDialog;
-import javax.swing.Timer;
-
-import Block.Block;
-import Block.Bonus;
-import Block.BonusBrick;
-import Block.Brick;
-import Ennemy.Champi;
-import Ennemy.Plant;
-import Ennemy.Turtle;
-
-import java.awt.event.KeyAdapter;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
-public class Mario  implements GameObject{
-    private int marioX, marioY;
-    private final int velX = 1;
-    private final int jumpSpeed = 15; 
-    private final int gravity = 1; 
-    private BufferedImage image; 
-    private boolean isRugbyman =true;
+public class Mario extends GameObject {
+    private boolean isRugbyman = true;
     private boolean interact = false;
-    private boolean moveLeft = false;
-    private boolean moveRight = false;
-    private boolean isJumping = false;
+
+    private boolean lookingRight = true;
     private double velY = 0;
     private double rotationAngle = 0;
 
-    public Mario(int X, int Y) {
-        super(X, Y);
-
-        sprite = Ressource.getImage("./img/mario1.png");
-
-
+    public Mario(int x, int y) {
+        super(x, y, "mario");
     }
-    public void jump(GameEngine engine) {
-        if(!isJumping() && !isFalling()){
+
+    public void jump() {
+        if (!isJumping() && !isFalling()) {
             setJumping(true);
             setVelY(10);
         }
     }
-    public void move(GameEngine engine){
-        if (moveLeft){
-            marioX -= velX;
-        }
-        if (moveRight)
-            marioX += velX;
+
+    public void move(boolean toRight) {
+        lookingRight = toRight;
     }
-    public void fall(GameEngine engine){
-        if (isJumping) {
-            marioY -= velY;
-            velY -= gravity;
-            if (marioY >= 150) { 
-                marioY = 150;
-                isJumping = false;
-                velY = 0;
-            }
-        }
-    }
-    public void powerup(GameEngine engine, PowerUp jersey){
-        if (this.collide(jersey)){
+//    public void fall(){
+//        if (isJumping()) {
+//            marioY -= velY;
+//            velY -= gravity;
+//            if (marioY >= 150) {
+//                marioY = 150;
+//                isJumping = false;
+//                velY = 0;
+//            }
+//        }
+//    }
+
+    //TODO: Handle power up in Game
+    public void powerup(PowerUp powerUp) {
+        if (this.collide(powerUp)) {
             isRugbyman = true;
             updateImage();
             Timer timer = new Timer(12000, new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     isRugbyman = false;
-                    updateImage(); 
-                    ((Timer)e.getSource()).stop(); 
+                    updateImage();
+                    ((Timer) e.getSource()).stop();
                 }
             });
-            timer.setRepeats(false); 
-            timer.start(); 
+            timer.setRepeats(false);
+            timer.start();
         }
     }
 
-    public void attack(GameEngine engine, Enemy enemy){
-         int timingCharge =30;
-         int regenCharge=300;
-        if (isRugbyman == true){
-            if (interact && regenCharge==300){
-                while(timingCharge >0)
-                {
+    public void attack(Enemy enemy) {
+        int timingCharge = 30;
+        int regenCharge = 300;
+        if (isRugbyman == true) {
+            if (interact && regenCharge == 300) {
+                while (timingCharge > 0) {
                     velX = 10;
                     timingCharge--;
 
                 }
                 regenCharge++;
 
-            if(this.collide(enemy)){
-                enemy.die();
-            }
-        }
-    }
-}
-public void Flag(GameEngine engine, Flag flag) {
-    int numClicks=0;
-    if (interact && this.collide(flag)) {
-        flag.breakFlag(); 
-
-        JOptionPane optionPane = new JOptionPane("Spam B", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
-        final JDialog dialog = new JDialog();
-        dialog.setTitle("Message");
-        dialog.setModal(true);
-
-        dialog.setContentPane(optionPane);
-        dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-        dialog.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_B) { 
-                    numClicks++;
+                if (this.collide(enemy)) {
+                    enemy.die();
                 }
             }
-        });
-
-        Timer timer = new Timer(5000, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();
-            } 
-        }); 
-        timer.setRepeats(false);
-        timer.start();
-
-        dialog.pack();
-        dialog.setVisible(true);
-
-        marioX += numClicks * 10;
-        numClicks = 0;
-    } 
-} 
-public void coins(Coin coin){
-    if (this.collide(coin)){
-        coin.disappear;
-    }
-}
-public void bricks(Block block){
-    if (this.collide(block) && block instanceof BonusBrick){
-        block.transform();
-        block.powerup();
-    }
-    if (this.collide(block)&& block instanceof Brick){
-        block.transform();
-    }
-    if (this.collide(block)&& block instanceof Bonus){
-        block.powerup();
-        block.transfrom();
-    }
-}
-public void die(Object enemy){
-    if (enemy instanceof Champi || enemy instanceof Turtle || enemy instanceof Plant){
-        if (this.collide(enemy)&& (horizontalHit)){
-            gameover
         }
     }
-}
-public void updateImage() {
-    if (isRugbyman){
-        sprite = Ressources.getImage("./img/marioRugby.png");  
-    } else {
-        sprite = Ressources.getImage("./img/mario1.png");  
+//public void Flag(Flag flag) {
+//    int numClicks=0;
+//    if (interact && this.collide(flag)) {
+//        flag.breakFlag();
+//
+//        JOptionPane optionPane = new JOptionPane("Spam B", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
+//        final JDialog dialog = new JDialog();
+//        dialog.setTitle("Message");
+//        dialog.setModal(true);
+//
+//        dialog.setContentPane(optionPane);
+//        dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+//        dialog.addKeyListener(new KeyAdapter() {
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//                if (e.getKeyCode() == KeyEvent.VK_B) {
+//                    numClicks++;
+//                }
+//            }
+//        });
+//
+//        Timer timer = new Timer(5000, new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                dialog.dispose();
+//            }
+//        });
+//        timer.setRepeats(false);
+//        timer.start();
+//
+//        dialog.pack();
+//        dialog.setVisible(true);
+//
+//        marioX += numClicks * 10;
+//        numClicks = 0;
+//    }
+//}
+
+    //public void coins(Coin coin){
+//    if (this.collide(coin)){
+//        coin.disappear;
+//    }
+//}
+//public void bricks(Block block){
+//    if (this.collide(block) && block instanceof BonusBrick){
+//        block.transform();
+//        block.powerup();
+//    }
+//    if (this.collide(block)&& block instanceof Brick){
+//        block.transform();
+//    }
+//    if (this.collide(block)&& block instanceof Bonus){
+//        block.powerup();
+//        block.transfrom();
+//    }
+//}
+//public void die(Object enemy){
+//    if (enemy instanceof Champi || enemy instanceof Turtle || enemy instanceof Plant){
+//        if (this.collide(enemy)&& (horizontalHit)){
+//            gameover
+//        }
+//    }
+//}
+    public void updateImage() {
+        if (isRugbyman) {
+            sprite = Ressources.getImage("marioStade");
+        } else {
+            sprite = Ressources.getImage("mario");
+        }
     }
-}
 }

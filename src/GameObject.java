5ec172d8be49fbd70c0db18e6987ffeba89c26f2
KurwaClown <CobraClown09300@outlook.fr>
2023-11-10 16:38:39 +0100

    [Update | GameObject] Add setter for jumping and falling

--- src/GameObject.java
+++ src/GameObject.java
@@ -87,10 +87,16 @@ public class GameObject {
         return jumping;
     }
 
+    public void setJumping(boolean jumping) {
+        this.jumping = jumping;
+    }
     public boolean isFalling() {
         return falling;
     }
 
+    public void setFalling(boolean falling) {
+        this.falling = falling;
+    }
     public void setX(double x) {
         this.x = x;
     }

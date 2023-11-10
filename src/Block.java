4c9d27ccf7f302cf8a0ab23c4a7bd8eb6a363fe9
Romain <romain@LAPTOP-FE662T13>
2023-11-10 15:09:07 +0100

    block v1

--- src/Block.java
+++ src/Block.java
@@ -4,20 +4,10 @@ public abstract class Block extends GameObject{
     public Block(double xLocation, double yLocation, BufferedImage sprite) {
         super(xLocation, yLocation, sprite);
     }
-        public void transform(Object mario){
-        if (mario instanceof Mario && hitUnder((Mario) mario)){
-            this.setImageIo("./img/deadbrick.png");
+        public void transform(){
+            sprite = Ressource.getImage("deadBrick");
         }
-        }
-    
-    private boolean hitUnder(Mario mario) {
-        boolean vertAlign = (mario.getY() + mario.getHeight()) == this.getY();
-    
-        boolean horiAlign = (mario.getX() < (this.getX() + this.getWidth()))
-                                        && ((mario.getX() + mario.getWidth()) > this.getX());
-    
-        return vertAlign && horiAlign;
     }
 
     
-}
+

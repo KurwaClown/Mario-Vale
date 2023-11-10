394a03d8591f54db3a4b991f89b9ee7b759e9fab
Romain <romain@LAPTOP-FE662T13>
2023-11-10 14:47:47 +0100

    Flag v1

--- src/Flag.java
+++ src/Flag.java
@@ -3,5 +3,9 @@ public class Flag extends GameObject{
 
     public Flag(double xLocation, double yLocation) {
         super(xLocation, yLocation, "flag");
+        sprite = Ressource.getImage("flag");
+    }
+    public void flagBreak(){
+        sprite=Ressource.getImage("flagBreak");
     }
 }

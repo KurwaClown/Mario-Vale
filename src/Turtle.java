462bf724b6068d2db85c74784303a0c54e8daf6e
Romain <romain@LAPTOP-FE662T13>
2023-11-10 13:14:06 +0100

    turtle v1

--- src/Turtle.java
+++ src/Turtle.java
@@ -1,7 +1,28 @@
 import java.awt.image.BufferedImage;
 
 public class Turtle extends GameObject{
-    public Turtle(double xLocation, double yLocation, BufferedImage sprite) {
-        super(xLocation, yLocation, sprite);
+    public int xInit;
+    private int direction = 1;
+    private int hp;
+    public Turtle (double xLocation, double yLocation) {
+        super(xLocation, yLocation);
+                 this.hp = 2;
+
+    }
+    public void move(Block block){
+        xLocation += direction* velX;
+         if (this.collide(block)){
+            direction = direction *(-1);
+         }
+    }
+    public void shell(Mario mario){
+        if (mario.collideFromTop(this) && hp==2){
+            sprite = Ressource.getImage("shell");
+            hp -=1;
+        }
+        else if (mario.collideFromTop(this) && hp==1){
+            this.yLocation = 3000;
+        }
     }
 }
+

ca4ca9cfab224f9353fcd0d79855b46e170c98ab
KurwaClown <CobraClown09300@outlook.fr>
2023-11-11 10:45:53 +0100

    [Update | Enemy] Updating turtle

--- src/Turtle.java
+++ src/Turtle.java
@@ -8,25 +8,23 @@ public class Turtle extends GameObject{
     private int direction = 1;
     private int hp;
     public Turtle (double xLocation, double yLocation) {
-        super(xLocation, yLocation);
+        super(xLocation, yLocation, "turtle");
                  this.hp = 2;
-                 sprite = Ressource.getImage("turtle");
-
     }
-    public void move(Block block){
-        xLocation += direction* velX;
-         if (this.collide(block)){
-            direction = direction *(-1);
-         }
-    }
-    public void shell(Mario mario){
-        if (mario.collideFromTop(this) && hp==2){
-            sprite = Ressource.getImage("shell");
-            hp -=1;
-        }
-        else if (mario.collideFromTop(this) && hp==1){
-            this.yLocation = 3000;
-        }
-    }
+//    public void move(Block block){
+//        x += direction* velX;
+//         if (this.collide(block)){
+//            direction = direction *(-1);
+//         }
+//    }
+//    public void shell(Mario mario){
+//        if (mario.collideFromTop(this) && hp==2){
+//            sprite = Ressource.getImage("shell");
+//            hp -=1;
+//        }
+//        else if (mario.collideFromTop(this) && hp==1){
+//            this.y = 3000;
+//        }
+//    }
 }
 

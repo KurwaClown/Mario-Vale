7faf6898260886ade793848efaca10d4687d8740
KurwaClown <CobraClown09300@outlook.fr>
2023-11-11 11:42:26 +0100

    [Add | Camera] Add base methods for Camera

--- src/Camera.java
+++ src/Camera.java
@@ -1,2 +1,24 @@
+import java.awt.*;
+
 public class Camera {
+    private double x = 0;
+    private double y = 0;
+
+    public Camera() {
+
+    }
+    public void moveCam(double xOffset, double yOffset){
+        x = x + xOffset;
+        y = y + yOffset;
+
+        System.out.println("Camera location = (" + x + ", " + y + ")");
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }

2f414ad8e1e2b765adcf329ef4b1c9c511044cc0
Ilan ATTIAS <CobraClown09300@outlook.fr>
2023-11-14 10:05:47 +0100

    [Update | Game] Shortening path

--- src/Game.java
+++ src/Game.java
@@ -25,7 +25,7 @@ public class Game {
         this.gameState = GameState.PLAYING;
         this.camera = new Camera();
         this.mario = new Mario(50, 700);
-        this.mapManager = new MapManager(camera, mario, "C:\\Users\\coraa\\IdeaProjects\\T-JAV-501-TLS_7\\out\\production\\T-JAV-501-TLS_7\\mapcsv\\map1.csv");
+        this.mapManager = new MapManager(camera, mario, ".\\out\\production\\T-JAV-501-TLS_7\\mapcsv\\map1.csv");
         this.map = mapManager.getMap();
         this.userInterface = new UserInterface(map);
 

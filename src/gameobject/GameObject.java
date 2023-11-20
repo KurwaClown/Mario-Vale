b174c4502ae81d8a868c7e781d9cb36dc5cf8a39
Ilan ATTIAS <CobraClown09300@outlook.fr>
2023-11-20 10:33:43 +0100

    [Refactor | GameObject] Cleaning Gameobject

--- src/gameobject/GameObject.java
+++ src/gameobject/GameObject.java
@@ -3,43 +3,71 @@ package gameobject;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-import gameobject.character.Mario;
-import gameobject.enemy.Turtle;
 import view.Ressource;
 
-public class GameObject {
+public abstract class GameObject {
     private double x, y;
     private double velX, velY;
-    private final double gravity = 0.30f;
     private boolean lookingRight = true;
-    private Dimension spriteDimension; // Dimension encapsulate width and height
+    private final Dimension spriteDimension = new Dimension(0, 0); // Dimension encapsulate width and height
     protected BufferedImage sprite;
-
     protected boolean jumping, falling;
 
-    public GameObject(double xLocation, double yLocation, String name){
+    public GameObject(double xLocation, double yLocation, String name) {
         setX(xLocation);
         setY(yLocation);
-        this.spriteDimension = new Dimension(64, 64);
-        setSprite(Ressource.getImage(name)); //picking the Sprite in Ressource.java
+        setSprite(Ressource.getImage(name)); //picking the Sprite in Resource.java
         setVelX(0);
         setVelY(0);
     }
 
 
-    /**
-     * @param g Graphics object
-     * Draws the sprite on the screen
-     */
-    public void draw(Graphics g) {
-        BufferedImage style = this.sprite;
-        if(!this.isLookingRight()) style = Ressource.getFlippedImage(this.sprite);
-        if(style != null){
-            g.drawImage(style, (int)getX(), (int)getY(), null);
+    public void moveObject() {
+        double GRAVITY = 0.30f;
+        if (isJumping() && getVelY() <= 0) { //After the apex of the jump
+            setJumping(false);
+            setFalling(true);
+        } else if (isJumping()) { // At jumping time
+            setVelY(getVelY() - GRAVITY);
+            setY(getY() - getVelY());
         }
+
+        if (isFalling()) { // When falling after apex
+            setY(getY() + getVelY());
+            setVelY(getVelY() + GRAVITY);
+
+        }
+        if (isLookingRight()) setX(getX() + getVelX());
+        else setX(getX() - getVelX());
     }
 
-    public void drawHitboxes(Graphics g){
+    // Creating Rectangle to check collisions (cf Game.java => Collisions management)
+    public Rectangle getBottomCollision() {
+        int hitboxHeight = isFalling() ? Math.max((int) getVelY(), 1) : 1;
+        return new Rectangle((int) getX(), (int) getY() + getSpriteDimension().height - hitboxHeight, getSpriteDimension().width, hitboxHeight);
+    }
+
+    public Rectangle getTopCollision() {
+        int hitboxHeight = isJumping() ? Math.max((int) getVelY(), 1) : 1;
+        return new Rectangle((int) getX(), (int) getY(), getSpriteDimension().width, hitboxHeight);
+    }
+
+    public Rectangle getLeftCollision() { //Yellow collision
+        int hitboxWidth = isLookingRight() ? 1 : Math.max((int) getVelX(), 1);
+        return new Rectangle((int) getX(), (int) getY(), hitboxWidth, getSpriteDimension().height);
+    }
+
+    public Rectangle getRightCollision() { //Pink collision
+        int hitboxWidth = isLookingRight() ? Math.max((int) getVelX(), 1) : 1;
+        return new Rectangle((int) getX() + getSpriteDimension().width - hitboxWidth, (int) getY(), hitboxWidth, getSpriteDimension().height);
+    }
+
+    public void draw(Graphics g) {
+        BufferedImage style = isLookingRight() ? getSprite() : Ressource.getFlippedImage(getSprite());
+        if (style != null) g.drawImage(style, (int) getX(), (int) getY(), null);
+    }
+
+    public void drawHitboxes(Graphics g) {
         g.setColor(Color.BLUE);
         g.fillRect(getTopCollision().x, getTopCollision().y, getTopCollision().width, getTopCollision().height);
         g.setColor(Color.GREEN);
@@ -50,45 +78,41 @@ public class GameObject {
         g.fillRect(getRightCollision().x, getRightCollision().y, getRightCollision().width, getRightCollision().height);
     }
 
-
-    public void moveObject(){
-        if(jumping && getVelY() <= 0){ //After the apex of the jump
-            jumping = false;
-            falling = true;
-        }
-        else if(jumping){ // At jumping time
-            setVelY(getVelY() - gravity);
-            setY(getY()- getVelY());
-        }
-
-        if(falling){ // When falling after apex
-            setY(getY() + getVelY());
-            setVelY(getVelY() + gravity);
-
-        }
-        if(lookingRight) setX(getX() + getVelX());
-        else setX(getX() - getVelX());
-    }
-    // Creating Rectangle to check collisions (cf Game.java => Collisions management)
-    public Rectangle getBottomCollision(){
-        int hitboxHeight = isFalling() ? Math.max((int)getVelY(),1) : 1;
-        return new Rectangle((int)getX(), (int)getY() + spriteDimension.height - hitboxHeight, spriteDimension.width, hitboxHeight);
+    public void drawData(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 12));
+        g.drawString(String.format("x: %.02f | y: %.02f", getX(), getY()), (int) getX(), (int) getY() - 20);
+        g.drawString(String.format("velX: %.02f | velY: %.02f", getVelX(), getVelY()), (int) getX(), (int) getY() - 10);
     }
 
-    public Rectangle getTopCollision(){
-        int hitboxHeight = isJumping() ? Math.max((int)getVelY(),1) : 1;
-        return new Rectangle((int)getX(), (int)getY(), spriteDimension.width, hitboxHeight);
-    }
-    
-    public Rectangle getLeftCollision(){ //Yellow collision
-        int hitboxWidth = isLookingRight() ? 1 : Math.max((int)getVelX(), 1);
-        return new Rectangle((int)getX(), (int)getY(), hitboxWidth, spriteDimension.height);
+    public void disappear() {
+        setY(3000);
+        setFalling(false);
     }
 
-    public Rectangle getRightCollision(){ //Pink collision
-        int hitboxWidth = isLookingRight() ? Math.max((int)getVelX(), 1) : 1;
-        return new Rectangle((int)getX() + spriteDimension.width - hitboxWidth, (int)getY(), hitboxWidth, spriteDimension.height);
+
+    public void setX(double x) {
+        this.x = x;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setPosition(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+
     public void setVelX(double velX) {
         this.velX = velX;
     }
@@ -105,53 +129,18 @@ public class GameObject {
         return velY;
     }
 
-    public boolean isJumping() {
-        return jumping;
-    }
-
     public void setJumping(boolean jumping) {
         this.jumping = jumping;
     }
-    public boolean isFalling() {
-        return falling;
-    }
 
+    public boolean isJumping() {
+        return jumping;
+    }
     public void setFalling(boolean falling) {
         this.falling = falling;
     }
-    public void setX(double x) {
-
-        this.x = x;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setSprite(BufferedImage sprite) {
-        this.sprite = sprite;
-        updateSpriteDimension();
-    }
-
-    public void updateSpriteDimension() {
-        this.spriteDimension.setSize(sprite.getWidth(), sprite.getHeight());
-    }
-
-    public Dimension getSpriteDimension() {
-        spriteDimension.setSize(sprite.getWidth(), sprite.getHeight());
-        return spriteDimension;
-    }
-
-    public Rectangle getHitbox() {
-        return new Rectangle((int)getX(), (int)getY(), sprite.getWidth(), sprite.getHeight());
+    public boolean isFalling() {
+        return falling;
     }
 
     public void setLookingRight(boolean lookingRight) {
@@ -161,25 +150,24 @@ public class GameObject {
     public boolean isLookingRight() {
         return lookingRight;
     }
-    public void UpdateSpriteDimension() {
-        this.spriteDimension = new Dimension(sprite.getWidth(), sprite.getHeight());
+    public void setSprite(BufferedImage sprite) {
+        this.sprite = sprite;
+        updateSpriteDimension();
     }
 
-    public void setPosition(int x, int y) {
-        setX(x);
-        setY(y);
+    public BufferedImage getSprite() {
+        return sprite;
     }
 
-    public void disappear(){
-        y = 3000;
-        setFalling(false);
+    public void updateSpriteDimension() {
+        this.spriteDimension.setSize(getSprite().getWidth(), getSprite().getHeight());
     }
 
-    public void drawData(Graphics g) {
-        g.setColor(Color.BLACK);
+    public Dimension getSpriteDimension() {
+        return spriteDimension;
+    }
 
-        g.setFont(new Font("Arial", Font.BOLD, 12));
-        g.drawString(String.format("x: %.02f | y: %.02f", getX(), getY()), (int)getX(), (int)getY()-20);
-        g.drawString(String.format("velX: %.02f | velY: %.02f", getVelX(), getVelY()), (int)getX(), (int)getY()-10);
+    public Rectangle getHitbox() {
+        return new Rectangle((int) getX(), (int) getY(), getSprite().getWidth(), getSprite().getHeight());
     }
 }
